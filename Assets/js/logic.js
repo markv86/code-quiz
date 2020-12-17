8496068241f1fc5686c56181258650ee8e1c8837
Mark Virostek <markv86@hotmail.com>
2020-12-17 09:50:33 -0500

    question title appears, working on showing question

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,4 +1,4 @@
-// timer countsdown from button click
+// timer countdown from button click / hide start screen div
 var count = 5;
 var counter = null;
 var startButton = document.getElementById("start");
@@ -14,6 +14,129 @@ function timer() {
      alert("Game Over");
   }
 }
-startButton.addEventListener("click", function() {
+// Event Listener to start the timer
+  startButton.addEventListener("click", function() {
     counter = setInterval(timer, 1000);
+  });
+
+// Hide Start-screen on click of the timer. JQuery
+$(document).ready(function(){
+  $("#start").click(function(){
+    $("#start-screen").hide();
+  });
 });
+
+
+// -------------------------------------------------------------------------------
+
+// setup variables for questions
+var progress = 0
+var test;
+var test_status;
+var question;
+var choice;
+var choices;
+var chA;
+var chB;
+var chC;
+var chD;
+var correct = 0;
+var incorrect = 0;
+
+// QUESTIONS ---------------------------------------------------------------------
+
+var codeQuestions = [
+  {
+      question: "Commonly used data types DO NOT include_____.",
+      a: "strings",
+      b: "booleans",
+      c: "alerts",
+      d: "numbers",
+      answer: "B"
+    },
+  {
+      question: "The condition in an if else statement is enclosed within _____.",
+      a: "quotes",
+      b: "curly braces",
+      c: "parenthesis",
+      d: "square brackets",
+      answer: "b"
+    },
+  {
+      question: "Arrays in Javascript can be used to store _____.",
+      a: "numbers and strings",
+      b: "other arrays",
+      c: "booleans",
+      d: "all the above",
+      answer: "d"
+    },
+  {
+    question: "String values must be enclosed within _____ when being assigned to a variable.",
+    a: "commas",
+    b: "curly braces",
+    c: "quotes",
+    d: "parenthesis",
+    answer: "d"
+    },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is _____.",
+    a: "Javascript",
+    b: "terminal / bash",
+    c: "for loops",
+    d: "console.log",
+    answer: "d"
+    }
+];
+// -------------------------------------------------------------------------------
+// Change "document.getElementById to variable get"
+function get(x) {
+  return document.getElementById(x);
+}
+// -------------------------------------------------------------------------------
+// Function to show question on page
+// *****NOT WORKING YET*****
+function showQuestion(){
+  test = get("test");
+  if(progress >= codeQuestions.length){
+    // I wanted to append to div, but it doesn't work
+    test.innerHTML = "You answered "+correct+" out of "+codeQuestions.length+" questions correct";
+    get("test_status").innerHTML = "Test completed";
+    progress = 0;
+    correct = 0;
+
+    return false;
+  }
+// counter on screen in "test_status" div
+  get("test_status").innerHTML = "Question "+(progress+1)+" of "+codeQuestions.length;
+  
+  question = codeQuestions[progress].question;
+  chA = codeQuestions[progress].a;
+  chB = codeQuestions[progress].b;
+  chC = codeQuestions[progress].c;
+  chD = codeQuestions[progress].d;
+
+  // shows question
+  test.innerHTML = "<h3>"+question+"</h3>";
+
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='D'> "+chD+"</label><br><br>";
+ test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+
+function checkAnswer(){
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+  if(choice == questions[pos].answer){
+    correct++;
+  }
+  pos++;
+  showQuestion();
+}
+
+document.getElementById("start").addEventListener("click",showQuestion);
